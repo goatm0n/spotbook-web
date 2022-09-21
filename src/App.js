7e51bc8e835a957760475e9839b87d256f022583
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2022-09-21 14:12:50 +0100

    init accounts dev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Accounts from './components/Accounts';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          fuck off change
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        
+    return (
+        <div className="App">
+            <Accounts></Accounts>
+        </div>
+    );
 }
 
 export default App;
